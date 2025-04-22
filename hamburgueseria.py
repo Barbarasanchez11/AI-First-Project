26fbdf3ae8174c6ab6534d5f71a7e5586cdc1725
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-04-22 14:05:23 +0200

    while loop

---
+++ hamburgueseria.py
@@ -0,0 +1,13 @@
+inventario = 100
+print(f'El inventario contiene {inventario} hamburguesas')
+
+#se utiliza cuando no sabemos cuando va a terminar el bucle#
+while inventario > 0:
+    if inventario <= 10:
+        print('Advertencia: El inventario está casi vacio.')
+    numHamburguesas = int(input('Cuantas hamburguesas quiere el cliente? '))
+    if numHamburguesas > inventario:
+        print(f'No hay suficientes hamburguesas en stock. Solo hay {inventario}')
+    else:
+        inventario -= numHamburguesas 
+        print(f'El cliente ha pedido {numHamburguesas} hamburguesas el inventario ahora tiene {inventario} hamburguesas')   
